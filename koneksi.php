933d0eb9a45342db61d6793b299e03913d731961
Hilmi Muhammad <hilmihafid@gmail.com>
2020-06-26 08:14:03 +0800

    Update koneksi.php
    change database name

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "db_ambar";
+$db_name = "ambar";
 
 $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die();
 
